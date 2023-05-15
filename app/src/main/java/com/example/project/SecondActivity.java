8ff94ca6fb4af0ae0545323f89a8af8204717abd
a22moamy <a22moamy@student.his.se>
2023-05-15 14:41:02 +0200

    Implementerade menu options i second activity så att användaren kan navigera sig tillbaka till main activity och vice versa.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -23,4 +26,39 @@ public class SecondActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         aboutWebView.loadUrl("file:///android_asset/about/index.html");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.home) {
+            showHome();
+            return true;
+        }
+
+        if (id == R.id.about) {
+            showAbout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showHome(){
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void showAbout(){ }
 }
