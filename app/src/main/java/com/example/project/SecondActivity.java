47c98ef099c898891e0c0832592e6ef6d908a1e4
a22moamy <a22moamy@student.his.se>
2023-05-15 14:38:19 +0200

    Skapade en webview i second activity som laddar den interna webbsidan.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,12 +3,24 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private WebView aboutWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        aboutWebView = findViewById(R.id.webview);
+        aboutWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = aboutWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        aboutWebView.loadUrl("file:///android_asset/about/index.html");
     }
 }
